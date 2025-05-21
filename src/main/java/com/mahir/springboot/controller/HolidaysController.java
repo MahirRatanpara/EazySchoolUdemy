2ad852ae517b81b3819706acd357d5c8d9c9c2c9
MR-1301 <mahir2cool@gmail.com>
2025-05-21 23:10:05 +0530

    example_26

--- src/main/java/com/mahir/springboot/controller/HolidaysController.java
+++ src/main/java/com/mahir/springboot/controller/HolidaysController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,7 +15,7 @@ import java.util.stream.Collectors;
 public class HolidaysController {
 
     @GetMapping("/holidays")
-    public String displayHolidays(Model model) {
+    public String displayHolidays(@RequestParam(required = false) Boolean festival, @RequestParam(required = false) Boolean federal, Model model) {
         List<Holiday> holidays = Arrays.asList(
                 new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
                 new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
@@ -30,6 +31,8 @@ public class HolidaysController {
             model.addAttribute(type.toString(),
                     (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
         }
+        model.addAttribute("festival", festival);
+        model.addAttribute("federal", federal);
         return "holidays.html";
     }
 }
